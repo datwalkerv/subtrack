f24b286e926b6ed9505ea166bf45e62fdfd0386b
datwalkerv <walkervmusic@gmail.com>
2025-10-04 13:59:42 +0200

    feat: static login/register

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,7 +1,59 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function RegisterPage() {
-    return ( 
-    <div className="flex items-center justify-center flex-1">
-        register
-    </div>
-    )
+  return (
+    <Card className="w-full max-w-sm bg-white/5">
+      <CardHeader>
+        <CardTitle className="text-center font-black">Sign Up</CardTitle>
+        <CardDescription className="text-center">
+          Create an account to start <br />
+          managing your subscriptions.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                type="text"
+                placeholder="Balint"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="balag@subtrack.co"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Sign Up
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 }
