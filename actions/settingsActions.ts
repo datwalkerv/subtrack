517f1523193b911c2713100bb130e240734e20e5
datwalkerv <walkervmusic@gmail.com>
2025-10-07 11:46:45 +0200

    feat: crud server action sample

---
+++ actions/settingsActions.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { auth, db } from "@/lib/auth/auth";
+import { settingsSchema } from "@/lib/validation/settingSchema";
+
+export function getSettings(formData: FormData) {
+  // TODO
+}
+
+export function updateSettings(formData: FormData) {
+  // TODO
+}
---
+++ actions/subscriptionsActions.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { auth, db } from "@/lib/auth/auth";
+import { subscriptionSchema } from "@/lib/validation/subscriptionSchema";
+
+export function createSubscription(formData: FormData) {
+  // TODO
+}
+
+export function getSubscriptions(formData: FormData) {
+  // TODO
+}
+
+export function updateSubscription(formData: FormData) {
+  // TODO
+}
+
+export function deleteSubscription(formData: FormData) {
+  // TODO
+}
+
